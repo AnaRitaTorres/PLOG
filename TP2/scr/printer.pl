7366df17b7514aac2a2b146472290697dc3d1e5b
RuiCS <rui.pedro.csoares@gmail.com>
2016-12-16 22:49:45 +0000

    Solver complete.

--- TP2/scr/printer.pl
+++ TP2/scr/printer.pl
@@ -2,15 +2,28 @@
 %%PRINTER%%
 %%%%%%%%%%%
 
+% TODO
+% A Predicate to print the board
+printElement(Elem) :- write(Elem).
+printBoard([]).
+printBoard([Row | Tail]) :-
+	printRow(Row), nl,
+	printBoard(Tail).
 
-printElement(Elem):-
-	number(Elem),
-	Elem > 0,
-	write(Elem).
-	
-printElement(Elem):-
-	number(Elem),
-	Elem == -1,
-	write(' ').
-	
-printElement(_).
+printRow([]).
+printRow([Elem | Tail]):-
+	printElement(Elem),
+	printRow(Tail).
+
+% A predicate to convert a list to a matrix of a given side.
+% Used to convert the flattened out solution back into an easier-to-print matrix.
+
+list_to_matrix([], _, []).
+list_to_matrix(List, Size, [Row|Matrix]):-
+  list_to_matrix_row(List, Size, Row, Tail),
+  list_to_matrix(Tail, Size, Matrix).
+
+list_to_matrix_row(Tail, 0, [], Tail).
+list_to_matrix_row([Item|List], Size, [Item|Row], Tail):-
+  NSize is Size-1,
+  list_to_matrix_row(List, NSize, Row, Tail).
--- TP2/scr/solver.pl
+++ TP2/scr/solver.pl
@@ -13,36 +13,53 @@
 % would have an arrow up, down, left or right of it. We don't need the direction
 % for the solution. Simply see that the length specified by the number + 1 
 % (this 1 is the square occupying the number) equals the total area of the region!
-% Then, we just ID the region and see all the possible regions...
+% Then, we just ID the region and see all the possible regions with restrictions...
 
-solver(Board, Result) :-
+solver(Board, Solution) :-
 	
 	% board is always square; calculate its side.	
 	length(Board, N),
 
+	% see where the numbers are. Assign them to a region (give them an index).
 	getIndexedNumbers(Board, Indexed),
 	length(Indexed, MaxRegions1),
 	MaxRegions #= MaxRegions1 - 1,
 
+	% create an empty matrix, to be restricted
 	createEmptySolution(N, MaxRegions, EmptyResult),
-	initRegions(EmptyResult, Indexed, InitedRegions),
 
-	constrainCells(Board, InitedRegions),
-	%constrainArea
+	% start off the regions where the numbers are on the original board (these are called capitals). Place the capital index on the matrix, not the length.
+	initRegions(EmptyResult, Indexed, Result),
 
-	flatten_levels(InitedRegions, Result),
-	labeling([], Result).
+	% See that cells above, below, left and right of these starting regions may only be occupied by these regions.
+	% Ie, a cell that's diagonal to a capital can never be on that capital's region.
+	constrainCells(Board, Result),
+
+	% The true problem of "Four Winds". Constrain the board so that it creates regions with areas said by the numbers.
+	constrainAreas(Indexed, Result, 0, MaxRegions1),
+
+	% The last two constraints would have made disjoint regions of the same index possible... This one takes those out of the solution, though.
+	constrainCohesions(Indexed, Result, 0, MaxRegions1),
+
+	% We were opperating on a matrix because it was a lot easier to visualize and manipulate. To generate the solution, we need to flatten out the matrix on a single list.
+	flatten_list(Result, Flat_Solution),
+
+	% Generate solution
+	labeling([], Flat_Solution),
+
+	% Convert back, so it's easier to print.
+	list_to_matrix(Flat_Solution, N, Solution).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%% AUXILARY PREDICATES %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% This predicate gets a matrix's element.
+% This predicate gets a matrix's element at a given pair of coordinates.
 getNumberAtCoord(-1,-1,-1,_).
 getNumberAtCoord(X, Y, Number, Board) :-
 	nth0(Y, Board, Row),
 	nth0(X, Row, Number).
 
-% This predicate sets a matrix's element.
+% This predicate sets a matrix's element at a given pair of coordinates.
 
 setNumberAtCoord(X, Y, Number, BoardIn, BoardOut) :- 	setLineAux(0,X,Y,Number,BoardIn,BoardOut), !.
 
@@ -63,7 +80,7 @@ setColAux(Xnow, X, Number, [Element | Tail], [Head | Tail2]) :-  Xnow \= X,
 																	setColAux(Xnow2, X, Number, Tail, Tail2).
 
 
-% This predicate gets all the puzzle's numbered squares, coupled with their (X, Y) coordinates
+% This predicate gets all the puzzle's numbered squares (capitals), coupled with their (X, Y) coordinates.
 
 getNumbers(Board, Numbers) :-
 	length(Board, N),
@@ -92,15 +109,16 @@ getNumbersInRow(Row, X, Y, N, Tail) :-
 
 % Auxiliary Predicate to flatten the list
 
-flatten_levels([], []).
-flatten_levels([HeadList| TailList],Result) :- 
-	flatten_levels(TailList,NewTailList), 
+flatten_list([], []).
+flatten_list( [HeadList| TailList], Result) :- 
+	flatten_list( TailList, NewTailList), 
 	!, 
-	append(HeadList,NewTailList,Result).
-flatten_levels([HeadList|Tail],[HeadList|OtherTail]) :- 
-	flatten_levels(Tail,OtherTail).
+	append(HeadList, NewTailList, Result).
+flatten_list( [ HeadList | Tail ], [ HeadList | OtherTail ]) :- 
+	flatten_list( Tail, OtherTail).
 
 % A predicate to add an index to each member of the squares list
+% Ie, this gets all the information we need about each capital: [Region_Index, Region_TotalArea, Region_CapitalX, Region_CapitalY].
 
 indexNumbers(Numbers, Indexed) :-
 	indexNumbersAux(Numbers, 0,Indexed).
@@ -110,13 +128,11 @@ indexNumbersAux([ NumbersHead | OtherNumbers ], Index, [IndexedHead | IndexedTai
 	Index1 is Index + 1,
 	indexNumbersAux(OtherNumbers, Index1, IndexedTail).
 
-% Do this from the board
-
 getIndexedNumbers(Board, Indexed) :-
 	getNumbers(Board, Numbers),
 	indexNumbers(Numbers, Indexed).
 
-% creates a NxN list
+% creates a NxN list (the empty solution). Also, constrain the domain from minRegion (always 0) to maxRegion (number of capitals).
 
 createEmptySolution(N, MaxRegions, Result) :-
 	createEmptySolutionAux(N, N, Result),
@@ -142,8 +158,8 @@ initRegions(Board, [[CurrIndex, _, X, Y] | Tail ], Result) :-
 	initRegions(NewBoard, Tail, Result).
 
 % get a list that, for a cell, gets the first obstacle in each vertical or horizontal direction
-% [VU, VD, HL, HR]. ex: [1, -1, 2, 1] means that region 1 is obstructing its right and up sides
-% and no region is obstructing its down side.
+% [[VU, VU_X, VU_Y], [VD, VD_X, VD_Y], [HL, HL_X, HL_Y], [HR, HR_X, HR_Y]]. Ie, the region VU, obstructs our cell at coordinates (VU_X, VU_Y). Same for all the other ones...
+% If our cell already belongs to a region, it gets all obstacles to that region.
 
 getObstacles(X, Y, Board, List) :-
 	getNumberAtCoord(X, Y, Number, Board),
@@ -202,6 +218,9 @@ getObstacle(X, Y, IncX, IncY, Board, Obstacle) :-
 
 
 % This is predicate that thins down the regions for any cells.
+% A cell can belong to any region above, below, left, and right of it (if they exist).
+% This thins down cells in the sense that it restrits the cell's domain to only accept those 4 (max) possible regions.
+% If nothing is obstructing the cell on any given side, that side does not interfere with the domain.
 
 constrainCells(Board, Regions) :-
 	length(Board, N),
@@ -217,8 +236,6 @@ constrainCols(Board, Regions, X, Y, N) :-
 	X1 #= X + 1,
 	constrainCols(Board, Regions, X1, Y, N).
 
-% A predicate to thin down the cell. Checks for the regions up, down, left and right of it, and sets its domain to those values.
-
 constrainCell(X, Y, Board, Regions) :-
 	getObstacles(X, Y, Board, [[OU_V, OU_X, OU_Y], [OD_V, OD_X, OD_Y], [OL_V, OL_X, OL_Y], [OR_V, OR_X, OR_Y]]),
 
@@ -231,3 +248,135 @@ constrainCell(X, Y, Board, Regions) :-
 
 	Var in {RegionUP,RegionDOWN,RegionLEFT,RegionRIGHT},
 	Var #>= 0.
+
+% This is the predicate that checks that all the regions on the board are acceptable (in area).
+% The area is the number given on the original board plus one (counting the capital as part of the region!).
+% It checks the cells and, if they are set to the region we want, they contribute to the region's total area.
+% This creates some impossible scenarios, though.
+
+constrainAreas(_,_, Length, Length).
+constrainAreas(Indexed, Regions, Index, Length) :-	
+	constrainArea(Index, Indexed, Regions, GottenArea),
+	IndexNew #= Index + 1,
+	constrainAreas(Indexed, Regions, IndexNew, Length).
+
+constrainArea(Index, Indexed, Regions, GottenArea) :-
+	member([Index, Area_1, _, _], Indexed),
+	Area #= Area_1 + 1,
+	getArea(Index, _, Regions, GottenArea), 
+	GottenArea #= Area.
+
+getArea(Index, Area, Regions, GottenArea) :-
+	length(Regions, N),
+	getAreaRows(0, N, Index, Regions, GottenArea).
+
+getAreaRows(N,N,_,_,0).
+getAreaRows(Y,N,Index,Regions,GottenArea) :-
+	getAreaCols(0,Y,N,Index,Regions,ThisGottenArea),
+	Y1 #= Y + 1,
+	getAreaRows(Y1, N, Index, Regions, OtherGottenArea),
+	GottenArea #= OtherGottenArea + ThisGottenArea.
+
+getAreaCols(N, _, N, _, _, 0).
+getAreaCols(X, Y, N, Index, Regions, GottenArea) :-
+
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #= Index #<=> B,
+
+	X1 #= X + 1,
+	getAreaCols(X1, Y, N, Index, Regions, ThisGottenArea),
+	GottenArea #= ThisGottenArea + B.
+
+% This predicate restricts the board so that no region can be disjoint with itself, ie, it removes scenarios like this one (numbers indicate region index):
+
+%	0 1 0 0 0
+%	0 1 1 1 1		Look at region 0. It's disjoint with itself.
+%	2 1 3 3 3
+%	2 4 4 4 4
+%	2 5 5 5 5
+
+%	0 0 0 0 0
+%	1 1 1 1 1		Should look like this.
+%	2 1 3 3 3
+%	2 4 4 4 4
+%	2 5 5 5 5
+
+constrainCohesions(_,_, Length, Length).
+constrainCohesions(Indexed, Regions, Index, Length) :-	
+	constrainCohesion(Indexed, Regions, Index),
+	IndexNew #= Index + 1,
+	constrainCohesions(Indexed, Regions, IndexNew, Length).
+
+constrainCohesion(Indexed, Regions, Index) :-
+	member([Index, Area_1, X, Y], Indexed),
+
+	constrainCohesionUp(Index, Regions, X, Y, AreaUp),
+	constrainCohesionDown(Index, Regions, X, Y, AreaDown),
+	constrainCohesionLeft(Index, Regions, X, Y, AreaLeft),
+	constrainCohesionRight(Index, Regions, X, Y, AreaRight),
+
+	OffsettedUp #= AreaUp - 1, OffsettedDown #= AreaDown - 1,
+	OffsettedLeft #= AreaLeft - 1, OffsettedRight #= AreaRight - 1,
+
+	Area_1 #= OffsettedUp + OffsettedDown + OffsettedLeft + OffsettedRight.
+
+constrainCohesionUp(_, _, _, -1, 0).
+constrainCohesionUp(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #= Index,
+
+	NewY #= Y - 1,
+	constrainCohesionUp(Index, Regions, X, NewY, NewArea),
+	Area #= NewArea + 1.
+constrainCohesionUp(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #\= Index,
+	Area #= 0.
+
+constrainCohesionDown(_, Regions, _, Y, 0) :- length(Regions, Y). 
+constrainCohesionDown(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #= Index,
+
+	NewY #= Y + 1,
+	constrainCohesionDown(Index, Regions, X, NewY, NewArea),
+	Area #= NewArea + 1.
+constrainCohesionDown(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #\= Index,
+	Area #= 0.
+
+constrainCohesionLeft(_, _, -1, _, 0).
+constrainCohesionLeft(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #= Index,
+
+	NewX #= X - 1,
+	constrainCohesionLeft(Index, Regions, NewX, Y, NewArea),
+	Area #= NewArea + 1.
+constrainCohesionLeft(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #\= Index,
+	Area #= 0.
+
+constrainCohesionRight(_, Regions, X, _, 0) :- length(Regions, X). 
+constrainCohesionRight(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #= Index,
+
+	NewX #= X + 1,
+	constrainCohesionRight(Index, Regions, NewX, Y, NewArea),
+	Area #= NewArea + 1.
+constrainCohesionRight(Index, Regions, X, Y, Area) :-
+	getNumberAtCoord(X, Y, Var, Regions),
+
+	Var #\= Index,
+	Area #= 0.
